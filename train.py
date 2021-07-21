5dcad498232c9a37586a73029cb95b81dcc0eec8
Farrukh Bulbulov <80750954+fbulbulov@users.noreply.github.com>
2021-07-21 19:29:00 +0200

    importig json intenets files to training model

---
+++ train.py
@@ -0,0 +1,14 @@
+import numpy as np
+import random 
+import json 
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset,DataLoader
+from nltk_utils import bag_of_words, tokenize, stem
+from model import NeuralNet #importing the model from model.py
+
+with open('intents.json', 'r') as f: #loading our json intents files
+    intents=json.load(f)
+
+
+
