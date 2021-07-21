8d4e31b8cccc3d46e761b3d1f1efa4395d3935b9
Farrukh Bulbulov <80750954+fbulbulov@users.noreply.github.com>
2021-07-21 20:27:16 +0200

    fixing json and training py

--- train.py
+++ train.py
@@ -3,12 +3,11 @@ import random
 import json 
 import torch
 import torch.nn as nn
+import nltk
 from torch.utils.data import Dataset,DataLoader
-from nltk_utils import bag_of_words, tokenize, stem
+#from nltk_utils import bag_of_words, tokenize, stem
 from model import NeuralNet #importing the model from model.py
 
 with open('intents.json', 'r') as f: #loading our json intents files
     intents=json.load(f)
-
-
 
